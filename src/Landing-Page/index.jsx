a92492f0ccd2ba55bc6506bd955a5cca1a449edd
Hady Safa <hadyabdallahsafa@gmail.com>
2024-10-01 15:56:50 +0300

    Some changes to the landing page

--- src/Landing-Page/index.jsx
+++ src/Landing-Page/index.jsx
@@ -8,6 +8,10 @@ import CV from '../assets/Hady Safa - Computer Engineering Student.pdf'
 function LandingPage() {
 
     const [viewImage,setViewImage] = useState(false)
+    const [width,setWidth] = useState()
+    const [height,setHeight] = useState()
+
+
     const imageDelay = 1000;
 
     useEffect( () => {
@@ -20,8 +24,23 @@ function LandingPage() {
         
     },[])
 
+    useEffect( () => {
+
+        function handlerFunction(e){
+            setWidth(window.innerWidth)
+            setHeight(window.innerHeight)
+        }
+
+        window.addEventListener("resize",handlerFunction)
+
+        return () => window.removeEventListener("resize",handlerFunction)
+        
+    },[])
+
     return (
         <section className={styles.page}>
+
+            <div className={styles.absolute}>{width} - {height}</div>
 
             <img src={Picture1} alt='Photo of me' className={`${styles.image} ${viewImage ? styles.visibleImage : styles.hiddenImage}`} />
 
@@ -30,12 +49,12 @@ function LandingPage() {
                 <div className={styles.info}>
                     <p>Hello, I am</p>
                     <h1 className={styles.name}>Hady Safa</h1>
-                    <p>-Computer Engineering Student-</p>
+                    <p>Computer Engineering Student</p>
                 </div>
 
                 <div className={styles.buttons}>
-                    <button className={styles.button}>About Me</button>
-                    <button className={styles.button}>Get In Touch</button>
+                    <a className={styles.button}>About Me</a>
+                    <a className={styles.button}>Get In Touch</a>
                     <a href={CV} target='_blank' className={styles.button}>CV</a>
                 </div>
 
