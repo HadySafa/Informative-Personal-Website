9ebf24cd4eff7b67dcd0a2b78295850544f192cb
Hady Safa <hadyabdallahsafa@gmail.com>
2024-10-06 22:07:23 +0300

    Initial code after adding projects section

--- src/Projects/index.jsx
+++ src/Projects/index.jsx
@@ -1,11 +1,107 @@
 import styles from './style.module.css'
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
+import { useState, useRef } from 'react'
+import { info } from './info'
 
 
-function Projects(){
+function Projects() {
 
-    return(
+    const [index, setIndex] = useState(0);
+    const infoArray = [...info];
+
+    const projectContainer = useRef()
+
+    function handleNextProject() {
+        if (index < 2) {
+            setIndex((i) => i + 1)
+
+            projectContainer.current.scrollBy({
+                top: 0,
+                left: projectContainer.current.clientWidth,
+                behavior: 'smooth'
+            });
+        }
+        else{
+            projectContainer.current.scrollBy({
+                top: 0,
+                left: -projectContainer.current.clientWidth  * 3,
+                behavior: 'smooth'
+            });
+            setIndex(0)
+        }
+    }
+
+    function handlePreviousProject() {
+        if (index > 0) {
+            setIndex((i) => i - 1)
+
+            projectContainer.current.scrollBy({
+                top: 0,
+                left: -projectContainer.current.clientWidth ,
+                behavior: 'smooth'
+            });
+        }
+        else{
+            projectContainer.current.scrollBy({
+                top: 0,
+                left: projectContainer.current.clientWidth  * 3,
+                behavior: 'smooth'
+            });
+            setIndex(2)
+        }
+    }
+
+    return (
         <section className={styles.page}>
-            
+
+            <div ref={projectContainer} className={styles.mainPageContentContainer}>
+                {
+                    infoArray.map((project, index) =>
+
+                        <div key={index} className={styles.project}>
+
+                            <img className={styles.projectImage} src={project.image} alt={project.alt} />
+
+                            <div className={styles.projectInfo}>
+
+                                <h2 className={styles.title}>{project.title}</h2>
+
+                                <p className={styles.description}>
+                                    {project.description}
+                                </p>
+
+                                <div className={styles.technologiesContainer}>
+                                    <h4 className={styles.technologiesHeader}>Technologies Used</h4>
+                                    <ul className={styles.technologiesList}>
+                                        {
+                                            project.technologies.map((element, index) => <li key={index}>{element}</li>)
+                                        }
+                                    </ul>
+                                </div>
+
+                                <div className={styles.buttons}>
+                                    <a target='_blank' href={project.link} className={styles.button}>View Website</a>
+                                    <a target='_blank' href={project.repo} className={styles.button}>Check Source Code</a>
+                                </div>
+
+                            </div>
+
+                        </div>
+                    )
+                }
+            </div>
+
+            <div className={styles.navigationContainer}>
+
+                <span onClick={handlePreviousProject} className={styles.icon}>< MdOutlineKeyboardArrowLeft /></span>
+
+                <p className={styles.index}>{index + 1} / {infoArray.length}</p>
+
+                <span onClick={handleNextProject} className={styles.icon}>< MdOutlineKeyboardArrowRight /></span>
+
+            </div>
+
         </section>
     )
 
---
+++ src/Projects/info.js
@@ -0,0 +1,35 @@
+import computerStore from '../assets/computerStore.png'
+import movieSpot from '../assets/theMovieSpot.png'
+import spotLightStore from '../assets/spotLightStore.png'
+
+
+
+export const info = [
+    {
+        image: movieSpot,
+        alt:'The Movie Spot Home Page',
+        title: "The Movie Spot",
+        description: 'A web application that allows users to easily browse and search through a wide collection of movies and series, view details, and save their preferred selections for quick access.',
+        technologies: ["ReactJS","Tailwind CSS","OMDB API"],
+        link: "https://master--the-movie-spot.netlify.app/",
+        repo: "https://github.com/HadySafa/The-Movie-Spot"
+    },
+    {
+        image: spotLightStore,
+        alt:'SpotLight Store Home Page',
+        title: "SpotLight Store",
+        description: 'A fully functional e-commerce website designed to provide users with a seamless shopping experience, where they can browse and search for products, and manage the cart.',
+        technologies: ["ReactJS","Tailwind CSS","React Router Dom","Redux"],
+        link: "https://master--trial-app.netlify.app/",
+        repo: "https://github.com/HadySafa/SpotLight-store"
+    },
+    {
+        image: computerStore,
+        alt:'Computer Store Home Page',
+        title: "Computer Store",
+        description: 'A website designed to allow users to browse laptops, manage the cart and reach the store.',
+        technologies: ["HTML5","CSS","Vanilla JS"],
+        link: "https://master--computer-store-app.netlify.app/",
+        repo: "https://github.com/HadySafa/Computer-store"
+    }
+]
