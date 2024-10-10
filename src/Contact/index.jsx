ce5e4dca78289df8554440672119068f83d93013
Hady Safa <hadyabdallahsafa@gmail.com>
2024-10-10 18:40:28 +0300

    Added github link

--- src/Contact/index.jsx
+++ src/Contact/index.jsx
@@ -4,6 +4,8 @@ import { FaLinkedin } from "react-icons/fa";
 import { IoMdMail } from "react-icons/io";
 import { FaPhone } from "react-icons/fa6";
 import { IoLogoWhatsapp } from "react-icons/io";
+import { LuGithub } from "react-icons/lu";
+
 
 function Contact() {
 
@@ -24,6 +26,12 @@ function Contact() {
                     href="https://www.linkedin.com/in/hadyabdallahsafa/"
                     title="Connect on LinkedIn!" className={styles.icon}><FaLinkedin /></a>
 
+                <a title="Visit my Github!"
+                    target="_blank"
+                    aria-label="Visit my Github!"
+                    href="https://github.com/HadySafa" className={styles.icon}><LuGithub />
+                </a>
+
                 <a aria-label="Send an Email!"
                     target="_blank"
                     href="mailto:hadyabdallahsafa@gmail.com"
@@ -37,7 +45,8 @@ function Contact() {
                 <a title="Text via Whatsapp!"
                     target="_blank"
                     aria-label="Text via Whatsapp !"
-                    href="https://wa.me/96170860816" className={styles.icon}><IoLogoWhatsapp /></a>
+                    href="https://wa.me/96170860816" className={styles.icon}><IoLogoWhatsapp />
+                </a>
 
             </div>
 
